b46325f7e88c0d1076de2fbe01ea9cd7ddd2b2c0
dheandry-hub <dheandry@student.ub.ac.id>
2026-09-23 16:25:57 +0700

    Add files via upload

---
+++ Utama.java
@@ -0,0 +1,30 @@
+package LatihanJava;
+public class Utama {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+int umur = 20;
+float tinggiBadan = 168.5f;
+char nilaiHuruf = 'A';
+boolean lulus = true;
+
+System.out.println("Umur: " + umur);
+System.out.println("Tinggi Badan: " + tinggiBadan);
+System.out.println("Nilai Huruf: " + nilaiHuruf);
+System.out.println("Lulus: " + lulus);
+
+var a = 5000;
+var b = 1000000;
+var c = a + b;
+float d = (float) a / b;
+long e = (long)a * b;
+
+System.out.println("Hasil penjumlahan: " + c);
+System.out.println("Hasil pembagian: " + d);
+System.out.println("Hasil perkalian: " + e);
+
+var A = 5;
+var t = 10;
+float luas = (0.5f * A * t);
+System.out.println("Luas: " + luas);
+    }
+}
